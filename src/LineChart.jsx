6f6d405df7f17e25aeb6eca18ea766fd47e10df8
Candie Hill <can330330@yahoo.com>
2022-08-29 05:05:31 -0400

    filtered data from ws

--- src/LineChart.jsx
+++ src/LineChart.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from "recharts";
 // promtp want to display current price on y axis and time on x axis
+//TODO - get the line graph to render data points
 function LineChartDisplay(props){
-    const [data, setData] = useState([]);
 
-
+    console.log(props.data)
     return (
         <div>
             <LineChart 
-                width={400}
-                height={400}
+                width={1000}
+                height={1000}
                 // data to be determined
-                data={data}
+                data={props.data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <CartesianGrid strokeDasharray="1" />
+                <CartesianGrid strokeDasharray="5" />
                 <XAxis dataKey="time" />
                 <YAxis dataKey="price"/>
                 <Tooltip />
                 <Legend />
-                <Line name="Bids" type="monotone" dataKey="buy" stroke="#8884d8" />
-                <Line name="Asks" type="monotone" dataKey="sell" stroke="#82ca9d" />
+                <Line name="bid" type="monotone" dataKey="bid" stroke="#000000" />
+                <Line name="ask" type="monotone" dataKey="ask" stroke="#82ca9d" />
             </LineChart>
         </div>
     )
--- src/StockPriceSize.jsx
+++ src/StockPriceSize.jsx
@@ -9,10 +9,16 @@ function SockPriceSize(props){
     const [data, setData] = useState([]);
     const [isPaused, setIsPaused] = useState(false);
     const webSocket = useRef(null)
+    const [bidsArr, setBidsArr] = useState([]);
+    const [asksArr, setAsksArr] = useState([]);
+    const [delay, setDelay] = useState(5000);
 
-    // to open websocket and send requst
-    setInterval(
+    // to open websocket and send requst 
     useEffect(() => {
+        //empty out arrays if there is a currency change
+        setBidsArr([])
+        setAsksArr([])
+        setData([])
         webSocket.current = new WebSocket("wss://ws-feed.exchange.coinbase.com");
         const request = {
             "type" : "subscribe",
@@ -35,57 +41,124 @@ function SockPriceSize(props){
         return () => {
             webSocketCurrent.close()
         }
-
     }, [props.currency])
-    ,5000)
+// if props currency changes empty the arrays and data sets
 
-
-    // to handle the incoming messages 
-    // setInterval(
+    // to handle the incoming messagess/grab all data
     useEffect(() => {
         if (!webSocket.current) return;
         webSocket.current.onmessage = (e) => {
             if (isPaused) {
-                return
+                return;
             };
             const msg = JSON.parse(e.data);
-            // every 15 sec get data get a snapshot or data update
+            // is this the first time we are collecting data
             if (msg.type === 'snapshot'){
-                const bestBidPrice = msg.bids[0][0]
-                const bestBidQty = msg.bids[0][1];
-                const bestAskPrice = msg.asks[0][0];
-                const bestAskQty = msg.asks[0][1];
+                setBestBidPrice(msg.bids[0][0]);
+                setBestBidQty(msg.bids[0][1]);
+                setBestAskPrice(msg.asks[0][0]);
+                setBestAskQty(msg.asks[0][1]);
+                // since snapshot is first, it will be the baseline of bidsArr,asksArr, and data array
+                setBidsArr(msg.bids);
+                setAsksArr(msg.asks);
                 const time = new Date();
                 const [hour, minutes, seconds] = [time.getHours(), time.getMinutes(), time.getSeconds()]
                 const bidData = {
                     transactionType: "bid",
-                    price: bestBidPrice,
+                    price: msg.bids[0][0],
                     time: `${hour}:${minutes}:${seconds}`
                 };
                 const askData = {
                     transactionType: "ask",
-                    price: bestAskPrice,
+                    price: msg.asks[0][0],
                     time: `${hour}:${minutes}:${seconds}`
                 };
-                setBestBidPrice(bestBidPrice);
-                setBestBidQty(bestBidQty);
-                setBestAskPrice(bestAskPrice);
-                setBestAskQty(bestAskQty);
-                setData(prevData => [...prevData, bidData, askData])
-            } 
-            
+                setData(() => [bidData, askData])
+
+            }
+            if (msg.type === "l2update"){
+                // iterate through changes
+                for (let i = 0; i < msg.changes.length; i++){
+                    const transactionType = msg.changes[i][0]
+                    const price = msg.changes[i][1];
+                    const size = msg.changes[i][2];
+                    if (transactionType === "buy"){
+                        // eliminate trades with size 0
+                        if (size === "0.00000000"){
+                            // filter out that price,size
+                            setBidsArr(bidsArr.filter(bid => bid[0] !== price))
+                        }else {
+                            setBidsArr(bids => [...bids, [price, size]])
+                        }
+                    }else {
+                        // you have a "sell"
+                        if (size === "0.00000000"){
+                            setAsksArr(asksArr.filter(ask => ask[0] !== price))
+                        }else {
+                            setAsksArr(asks => [...asks, [price,size]])
+                        }
+                    }
+                }
+            }
+        
         }
     },[isPaused, props.currency])
-    // ,5000)
 
+    // creating custom callback since setInterval is rendering incorrectly
+    function useInterval(callback,delay){
+        const savedCallback = useRef();
 
-        
-    
+        useEffect (() => {
+            savedCallback.current = callback;
+        },[callback]);
 
-    console.log(data)
+        useEffect(() => {
+            function sortData(){
+                savedCallback.current();
+            }
+            // if delay is not null ok to setup
+            if(delay !== null){
+                let id = setInterval(sortData, delay);
+                return () => clearInterval(id);
+            }
+        },[delay])
+    }
+    useInterval(() => {
+        // sort both arrays based off of highest bid and lowest ask prices     
+        bidsArr.sort((a,b) => b[0] - a[0])
+        asksArr.sort((a,b) => a[0] - b[0])
+
+        // update best bid and ask states
+        setBestBidPrice(bidsArr[0][0]);
+        setBestBidQty(bidsArr[0][1]);
+        setBestAskPrice(asksArr[0][0]);
+        setBestAskQty(asksArr[0][1]);
+        // eventually pass best bid, best ask (prices), and time to line graph as data
+        //Question - should I use current time or trade time? Also, how do I get this to update like every 15sec starting from 00 to 15 to 30 to 45 or is this a feature of the line graph/rechart library?
+        const time = new Date();
+        const [hour, minutes, seconds] = [time.getHours(), time.getMinutes(), time.getSeconds()]
+        const bidData = {
+            transactionType: "bid",
+            price: bestBidPrice,
+            time: `${hour}:${minutes}:${seconds}`
+        };
+        const askData = {
+            transactionType: "ask",
+            price: bestAskPrice,
+            time: `${hour}:${minutes}:${seconds}`
+        };
+        setData(prevdata => [...prevdata, bidData, askData])
+        // console.log('bidsArr.length', bidsArr.length)
+
+    }, !isPaused ? delay : null);
+
+    function handlePause(){
+        !isPaused ? setIsPaused(true) : setIsPaused(false);
+    }
+
     return( 
         <div>
-            <button onClick={() => setIsPaused(!isPaused)}>
+            <button onClick={handlePause}>
                 {isPaused ? "Continue Data Feed" : "Stop Data Feed"}
             </button>
             <div>
@@ -95,9 +168,10 @@ function SockPriceSize(props){
                 <ul>Best Ask Qty {bestAskQty}</ul>
             </div>
             <div>
-                {/* <LineChartDisplay currency={props.currency} data={data}/> */}
+                <LineChartDisplay currency={props.currency} data={data}/>
             </div>
         </div>
     );
 }
 export default SockPriceSize;
+
